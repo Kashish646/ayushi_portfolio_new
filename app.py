8c5cc8170bbcf32090398e4be5b658bee5663df1
Kashish646 <162801366+Kashish646@users.noreply.github.com>
2024-03-25 12:56:28 +0530

    Update app.py
    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session,flash
 
 
-app = Flask(__name__, static_folder='/static')
+app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 def get_flashed_messages():
